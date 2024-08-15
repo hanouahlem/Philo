d98e1a9821853e75e832a7e030ea0d5aff888b0e
Ahlem <131889727+hanouahlem@users.noreply.github.com>
2024-08-15 19:08:03 +0200

    Update init_philo.c
    modif usec

--- init_philo.c
+++ init_philo.c
@@ -55,7 +55,7 @@ long	get_current_time(void)
 	struct timeval	time;
 
 	gettimeofday(&time, NULL);
-	return ((time.tv_sec * 1000) + (time.tv_sec / 1000));
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
 
 
